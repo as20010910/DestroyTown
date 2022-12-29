b6c9c964e945ae0989d63821fcaa0f095d0e818f
unknown <108314141+HirooSugiyama@users.noreply.github.com>
2022-12-30 00:00:13 +0900

    プロジェクト移行
    BPのプロジェクトからC++も使えるプロジェクトに移行しました。
    それに伴いPlayFabのプラグインの追加を行いました。
    その他、細かな調整を行いました。

---
+++ Source/DestroyTown/DestroyTown.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DestroyTown.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DestroyTown, "DestroyTown" );
